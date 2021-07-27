638e377783c4646dd63b4f7d5199c9e2d3c44ae9
anshugupta27 <rush2anshugupta@gmail.com>
2021-07-27 19:56:16 +0530

    mavenize

--- frontend/src/main/solrFrontend/src/app/service/api-service.service.ts
+++ frontend/src/main/solrFrontend/src/app/service/api-service.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 export class ApiServiceService {
 
   constructor(private httpClient: HttpClient) { }
-   public backendRoute = 'http://localhost:9090/spring-solr-email-0.0.1-SNAPSHOT/api/v1' ;
+   public backendRoute = '/spring-solr-email-0.0.1-SNAPSHOT/api/v1' ;
    public message!:Email[] ;
 
   setMessage ( emails:any )
